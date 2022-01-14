c94185523a90a895ae3ab80d3040bfc45fa9067c
DaboRestrepo <daborestrepo@gmail.com>
2022-01-14 12:16:25 -0500

    remove the delim -

--- read_line.c
+++ read_line.c
@@ -18,7 +18,7 @@ void read_line(FILE *fd, stack_t *stack)
 		{
 			if ((_strcmp(str, push)) == 0)
 			{
-				arg = strtok(NULL, "-\n\t\r ");
+				arg = strtok(NULL, "\n\t\r ");
 				printf("%s\n", arg);
 				if (arg == NULL || (is_digit(arg)) == -1)
 				{
