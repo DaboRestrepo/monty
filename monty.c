62417c7063c047f354b1c937f39fa56932809587
DaboRestrepo <daborestrepo@gmail.com>
2022-01-14 11:50:29 -0500

    Fix the error monty file

--- monty.c
+++ monty.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	stack_t *stack = NULL;
 	FILE *fd = NULL;
 
-	if (argc == 0 || argc > 2)
+	if (argc <= 1 || argc > 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
